481202372898912cc63df9b8e88d3d0536d23b50
unknown <adailsonacj@live.com>
2020-08-15 12:23:14 -0300

    alterações feeitas em rotas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,59 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
+app.get('/repositories', (request, response) => {
+  // TODO
+  return response.json({ repositories });
+});
+
+app.post('/repositories', (request, response) => {
+  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    title,
+    url,
+    techs,
+    likes: 0,
+    id: uuid(),
+  };
+  repositories.push(repository);
+  return response.status(201).json({ repository });
+});
+
+app.put('/repositories/:id', (request, response) => {
+  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  const repositoryEdited = { title, url, techs };
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    ...repositoryEdited,
+  };
+  return response
+    .status(201)
+    .json({ repository: repositories[repositoryIndex] });
+});
+
+app.delete('/repositories/:id', (request, response) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
   // TODO
-});
-
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
-
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    likes: repositories[repositoryIndex].likes + 1,
+  };
+  return response
+    .status(201)
+    .json({ repository: repositories[repositoryIndex] });
 });
 
 module.exports = app;
